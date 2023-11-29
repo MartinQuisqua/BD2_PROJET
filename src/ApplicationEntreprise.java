09f6ee9535efcc5fb7a8a1ce8b0014f3a63133fe
Quisquater Martin <martin.quisquater@student.vinci.be>
2023-11-29 15:07:34 +0100

    premier commit depuis longtemps

--- src/ApplicationEntreprise.java
+++ src/ApplicationEntreprise.java
@@ -4,10 +4,13 @@ import java.util.Scanner;
 public class ApplicationEntreprise {
     private String url = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=Mama@0202";
     private Connection connection = null;
-    static PreparedStatement connexionEntrepriseSql = null;
-    static PreparedStatement encoderOffreStage = null;
-    static Scanner scanner = new Scanner(System.in);
-    static String idEntreprise;
+    private PreparedStatement connexionEntrepriseSql = null;
+    private PreparedStatement encoderOffreStage = null;
+    private PreparedStatement affichageMotClefs = null;
+    private PreparedStatement ajouterMotClefs = null;
+    private PreparedStatement voirSesOffres = null;
+    private Scanner scanner = new Scanner(System.in);
+    private String idEntreprise;
 
     public void ProgrammePrincipal() {
         try {
@@ -24,29 +27,29 @@ public class ApplicationEntreprise {
         }
         try {
             connexionEntrepriseSql = connection.prepareStatement("SELECT mdp_hash, code FROM Projet_BD2.entreprises WHERE email = ?;");
+            encoderOffreStage = connection.prepareStatement("SELECT Projet_BD2.ajouterStage(?,?,?);");
+            affichageMotClefs = connection.prepareStatement("SELECT * FROM Projet_BD2.affichage_mot_clefs;");
+            ajouterMotClefs = connection.prepareStatement("SELECT Projet_BD2.ajouterMotClef(?,?,?);");
+            voirSesOffres = connection.prepareStatement("SELECT * FROM Projet_BD2.voir_offres_des_stages WHERE code_entreprise = ?;");
         } catch (SQLException e) {
             System.out.println("Impossible de préparer la requête !");
             System.exit(1);
         }
-        try {
-            encoderOffreStage = connection.prepareStatement("Projet_BD2.ajouterStage(?,?,?);");
-        } catch (SQLException e) {
-            System.out.println("Impossible de préparer la requête !");
-            System.exit(1);
-        }
+
     }
 
     public static void main(String[] args) {
         ApplicationEntreprise app = new ApplicationEntreprise();
         app.ProgrammePrincipal();
-        connexionEntreprises();
+        app.connexionEntreprises();
     }
-    public static void connexionEntreprises() {
+    public void connexionEntreprises() {
         System.out.println("***************** Connexion entreprise *****************");
         System.out.println("Veuillez entrer votre nom d'utilisateur");
-        String email = "Entreprise1.test@Entreprise.test"; //scanner.nextLine(); // TODO: 07/05/2021 "
+        String email = "Entreprise3@gmail.test"; //scanner.nextLine(); // TODO: 07/05/2021 "
         System.out.println("Veuillez entrer votre mot de passe");
-        String passwordUser = scanner.nextLine();
+        // String passwordUser = scanner.nextLine();
+        String passwordUser = "Entreprisetest"; // TODO: 07/05/2021
 
         try {
             connexionEntrepriseSql.setString(1, email);
@@ -77,7 +80,7 @@ public class ApplicationEntreprise {
         }
     }
 
-    public static void applicationCentrale() {
+    public void applicationCentrale() {
         System.out.println("idEntreprise : " + idEntreprise);
         System.out.println("***************** application entreprise *****************");
         System.out.println("1 :Encoder une offre de stage");
@@ -88,6 +91,7 @@ public class ApplicationEntreprise {
         System.out.println("6 :Sélectionner un étudiant pour une de ses offres de stage");
         System.out.println("7 :Annuler une offre de stage en donnant son code");
         int choix = scanner.nextInt();
+        scanner.nextLine();
         switch (choix) {
             case 1:
                 encoderOffre();
@@ -118,19 +122,85 @@ public class ApplicationEntreprise {
 
 
 
-    private static void encoderOffre() {
+    private void encoderOffre() {
+        System.out.println("***************** Encoder une offre de stage *****************");
+        System.out.println("Veuillez entrer une description de l'offre de stage :");
+        String description = scanner.nextLine();
+        System.out.println("description : " + description);
+        System.out.println("Veuillez entrer le semestre de l'offre de stage");
+        String semestre = scanner.nextLine();
+        try {
+            encoderOffreStage.setString(1, idEntreprise);
+            encoderOffreStage.setString(2, description);
+            encoderOffreStage.setString(3, semestre);
+            encoderOffreStage.executeQuery();
+            System.out.println("Offre de stage ajoutée !");
+            applicationCentrale();
+        } catch (SQLException se) {
+            System.out.println("Erreur lors de l’insertion !");
+            se.printStackTrace();
+            System.exit(1);
+        }
+    }
+    private void voirLesMotsCLefs() {
+        try {
+            affichageMotClefs.execute();
+            ResultSet rs = affichageMotClefs.getResultSet(); // Utilisez executeQuery au lieu de getResultSet
+            System.out.println("  mot clefs disponible");
+            System.out.println("_______________");
+            while (rs.next()) {
+                System.out.printf("|  %-10s  | \n", rs.getString("mot")); // Utilisez le nom de la colonne "mot"
+            }
+            System.out.println("_______________");
+            applicationCentrale();
+        } catch (SQLException se) {
+            se.printStackTrace();
+            System.out.println("Aucun cours disponible");
+        }
+    }
 
+    private void ajouterMotClefs() {
+        System.out.println("***************** Ajouter un mot-clé à une de ses offres de stage *****************");
+        System.out.println("Veuillez entrer le code de l'offre de stage :");
+        String code = scanner.nextLine();
+        System.out.println("Veuillez entrer le mot clé à ajouter :");
+        String motClef = scanner.nextLine();
+        try {
+            ajouterMotClefs.setString(1, code);
+            ajouterMotClefs.setString(2, motClef);
+            ajouterMotClefs.setString(3, idEntreprise);
+            ajouterMotClefs.executeQuery();
+            System.out.println("Mot clé ajouté !");
+            applicationCentrale();
+        } catch (SQLException se) {
+            System.out.println("Erreur lors de l’insertion !");
+            se.printStackTrace();
+            System.exit(1);
+        }
     }
-    private static void ajouterMotClefs() {
+    private void voirSesOffres() {
+        try {
+            voirSesOffres.setString(1, idEntreprise);
+            voirSesOffres.execute();
+            ResultSet rs = voirSesOffres.getResultSet();
+            System.out.println("  Offre de stage");
+            System.out.println("_______________________________________________________________________________________________________|");
+            System.out.println("  | code stage |  Description   |Semestre|    etat     |nombre candidature en attentre |     nom       |");
+            System.out.println("_______________________________________________________________________________________________________|");
+            while (rs.next()) {
+                System.out.printf("  |   %-7s  |   %-10s   |  %-5s | %-10s | %-10s                    | %-10s |\n", rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6));
+            }
+            System.out.println("____________________________");
+            applicationCentrale();
+        } catch (SQLException se) {
+            se.printStackTrace();
+            System.out.println("Aucun cours disponible");
+        }
     }
-    private static void voirLesMotsCLefs() {
+    private void voirSesCandidature() {
     }
-    private static void voirSesOffres() {
+    private void selectionnerEtudiant() {
     }
-    private static void voirSesCandidature() {
-    }
-    private static void selectionnerEtudiant() {
-    }
-    private static void annulerOffreDeStage() {
+    private void annulerOffreDeStage() {
     }
 }
