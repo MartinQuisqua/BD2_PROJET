d4450c60965c4dbed5e0bf78560caac158441a63
Brulans4 <118511976+Brulans4@users.noreply.github.com>
2023-12-07 12:57:58 +0100

    Update ApplicationEtudiant.java

--- src/ApplicationEtudiant.java
+++ src/ApplicationEtudiant.java
@@ -32,9 +32,9 @@ public class ApplicationEtudiant {
         try {
             chercherOffresStageValidees = connection.prepareStatement("SELECT * FROM Projet_BD2.offresStageValideesParEtudiant WHERE id_etudiant = ?;");
             chercherOffresStageMotCle = connection.prepareStatement("SELECT * FROM Projet_BD2.rechercheOffresStageMotClef WHERE id_etudiant = ? AND mot_clef = ?;");
-            poserCandidature = connection.prepareStatement("SELECT Projet_BD2.poserCandidature(?,?,?)");
+            poserCandidature = connection.prepareStatement("SELECT Projet_BD2.poserCandidature(?,?,?);");
             chercherOffresCandidature = connection.prepareStatement("SELECT * FROM Projet_BD2.offresCandidature WHERE etudiant = ?;");
-            annulerCandidature = connection.prepareStatement("SELECT Projet_BD2.annuler_candidature(?,?)");
+            annulerCandidature = connection.prepareStatement("SELECT Projet_BD2.annuler_candidature(?,?);");
             connexionEtudiant = connection.prepareStatement("SELECT mdp_hash, id_etudiant FROM Projet_BD2.etudiants WHERE email = ?;");
         } catch (SQLException e) {
             System.out.println("Impossible de prÃ©parer la requÃªte !");
